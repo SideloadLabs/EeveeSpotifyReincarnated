b570335def1d6c11f129f26d7427c731b09ac9b1
Charles L <Charles_L@outlook.jp>
2026-05-21 02:55:55 +0800

    Add translatedLyrics and yrcLyrics fields to LrclibSong

--- Sources/EeveeSpotify/Lyrics/Models/Lrclib/LrclibSong.swift
+++ Sources/EeveeSpotify/Lyrics/Models/Lrclib/LrclibSong.swift
@@ -4,5 +4,7 @@ struct LrclibSong: Decodable {
     var name: String
     var plainLyrics: String?
     var syncedLyrics: String?
+    var yrcLyrics: String?  // 逐字歌词字段
     var instrumental: Bool
+    var translatedLyrics: String?
 }
